009864c7e2c5e2fb660a16fed5f3a0ad32a0ce08
Andres Lopez Lucero <luloandry@Andress-Mac-mini.local>
2026-05-13 21:32:37 -0500

    feat(tables_definition): update flask config, create EER diagram, add the mysql script of table creation

--- src/flask_hospital/__init__.py
+++ src/flask_hospital/__init__.py
@@ -1,24 +1,20 @@
-from typing import TYPE_CHECKING
-
 from flask import Flask
 
 from flask_hospital.config import DevelopmentConfig
-from flask_hospital.db import create_db
+from flask_hospital.db import db
 from flask_hospital.routes.index.routes import index_bp
-
-if TYPE_CHECKING:
-    from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app() -> Flask:
-    app = Flask(__name__)
+    app: Flask = Flask(__name__)
 
     app.config.from_object(DevelopmentConfig)
 
-    db: SQLAlchemy = create_db()
     db.init_app(app)
 
     with app.app_context():
+        from flask_hospital import models  # type: ignore[import-untyped]  # noqa: F401, PLC0415
+
         db.create_all()
 
     app.register_blueprint(index_bp)
